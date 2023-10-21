d458c16f641bcf23c22852d406534bb8fb7c1df7
Matt Manning <mtmanning93@hotmail.co.uk>
2023-10-21 13:13:09 +0000

    Add navbar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,28 @@
 import React from "react";
+import styles from "../styles/NavBar.module.css";
 import { Nav, Navbar, Container, Button } from "react-bootstrap";
 import logo from "../assets/main_logo.png";
 
 const NavBar = () => {
   return (
-    <Container fluid>
-      <Navbar expand="md" className="">
+    <Navbar expand="md" className={styles.NavBar}>
+      <Container fluid>
         <Navbar.Brand>
           <img src={logo} alt="logo" height="60" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-right">
-            <Nav.Link>Home</Nav.Link>
-            <Nav.Link>Login</Nav.Link>
+            <Nav.Link className={styles.Link}>Home</Nav.Link>
+            <Nav.Link className={styles.Link}>Login</Nav.Link>
             {/* Button to be seperate component */}
-            <Button variant="warning"><i className="fas fa-user-plus"></i> Sign up</Button> 
+            <Button className={`${styles.Link} ${styles.buttonStyle}`} variant="warning">
+              <i className="fas fa-user-plus"></i> Sign up
+            </Button>
           </Nav>
         </Navbar.Collapse>
-      </Navbar>
-    </Container>
+      </Container>
+    </Navbar>
   );
 };
 
