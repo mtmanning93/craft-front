35618f9c4a3dbe4b9a661c3b8f9d257fedae118a
Matt Manning <mtmanning93@hotmail.co.uk>
2023-10-21 13:25:31 +0000

    Install react router, and enable it in index.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import styles from './App.module.css';
-import NavBar from './components/NavBar';
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import { Container } from "react-bootstrap";
+
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route exact path="/" render={() => <h1>Sign In</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
