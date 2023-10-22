c0210f65f087bfbe5a6b4f025bbbca4a3d0d55ee
Matt Manning <mtmanning93@hotmail.co.uk>
2023-10-22 15:24:04 +0000

    Add final styles to SignUpForm component, maual testing carried out, close #3

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,39 +1,49 @@
 import React from "react";
 import styles from "../styles/NavBar.module.css";
-import { Nav, Navbar, Container, Button } from "react-bootstrap";
+import { Nav, Navbar, Button } from "react-bootstrap";
 import logo from "../assets/main_logo.png";
 import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" className={styles.NavBar}>
-      <Container fluid>
-        <NavLink to="/">
-          <Navbar.Brand>
-            <img src={logo} alt="logo" height="60" />
-          </Navbar.Brand>
-        </NavLink>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto text-right">
-            <NavLink exact to="/" className={styles.Link} activeClassName={styles.Active}>
-              Home
-            </NavLink>
-            <NavLink to="/login" className={styles.Link} activeClassName={styles.Active}>
-              Login
-            </NavLink>
-            <NavLink to="/signup">
-              {/* Button to be seperate component */}
-              <Button
-                className={`${styles.Link} ${styles.buttonStyle}`}
-                variant="warning"
-              >
-                <i className="fas fa-user-plus"></i> Sign up
-              </Button>
-            </NavLink>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+    <Navbar expand="md" className={`${styles.NavBar}`}>
+      <NavLink to="/">
+        <Navbar.Brand className="mx-auto">
+          <img src={logo} alt="logo" height="60" />
+        </Navbar.Brand>
+      </NavLink>
+      <Navbar.Toggle
+        className={styles.Toggler}
+        aria-controls="basic-navbar-nav"
+      />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto text-right">
+          <NavLink
+            exact
+            to="/"
+            className={styles.Link}
+            activeClassName={styles.Active}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/login"
+            className={styles.Link}
+            activeClassName={styles.Active}
+          >
+            Login
+          </NavLink>
+          <NavLink to="/signup">
+            {/* Button to be seperate component */}
+            <Button
+              className={`${styles.Link} ${styles.buttonStyle} mr-0`}
+              variant="warning"
+            >
+              <i className="fas fa-user-plus"></i> Sign up
+            </Button>
+          </NavLink>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
