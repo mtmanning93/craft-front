0905ab3eed5d9a19aaf0fbaaef08b11de4265c5f
Matt Manning <mtmanning93@hotmail.co.uk>
2023-10-23 10:12:38 +0000

    Add current user context to app.js, and implement the handleMount function

--- src/App.js
+++ src/App.js
@@ -2,25 +2,49 @@ import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import { Container } from "react-bootstrap";
 import { Route, Switch } from "react-router-dom";
-import './api/axiosDefaults';
+import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import Header from "./components/Header";
 import LogInForm from "./pages/auth/LogInForm";
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const handleMount = async () => {
+    try {
+      const { data } = await axios.get("dj-rest-auth/user/");
+      setCurrentUser(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    handleMount();
+  }, []);
+
   return (
-    <div className={styles.App}>
-      <NavBar />
-      <Header />
-      <Container>
-        <Switch>
-          <Route exact path="/" render={() => <h1>Home</h1>} />
-          <Route exact path="/login" render={() => <LogInForm />} />
-          <Route exact path="/signup" render={() => <SignUpForm />} />
-          <Route render={() => <h1>Page not found!</h1>} />
-        </Switch>
-      </Container>
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
+        <div className={styles.App}>
+          <NavBar />
+          <Header />
+          <Container>
+            <Switch>
+              <Route exact path="/" render={() => <h1>Home</h1>} />
+              <Route exact path="/login" render={() => <LogInForm />} />
+              <Route exact path="/signup" render={() => <SignUpForm />} />
+              <Route render={() => <h1>Page not found!</h1>} />
+            </Switch>
+          </Container>
+        </div>
+      </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
   );
 }
 
