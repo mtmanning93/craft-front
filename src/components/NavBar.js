efa73880c5da82f54466b3f8a62719025a7127d9
Matt Manning <mtmanning93@hotmail.co.uk>
2023-10-23 17:42:35 +0000

    Create custom hook to toggle burger menu icon on click outside or link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from "../styles/NavBar.module.css";
 import { Nav, Navbar, Button } from "react-bootstrap";
 import logo from "../assets/main_logo.png";
@@ -9,10 +8,14 @@ import {
 } from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
+import ClickOutsideToggle from "../hooks/ClickOutsideToggle";
 
 const NavBar = () => {
+
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+
+  const {expanded, setExpanded, ref} = ClickOutsideToggle();
 
   const handleLogOut = async () => {
     try {
@@ -68,15 +71,17 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar expand="md" className={`${styles.NavBar}`}>
+    <Navbar expanded={expanded} expand="md" className={`${styles.NavBar}`}>
       <NavLink to="/">
         <Navbar.Brand className="mx-auto">
           <img src={logo} alt="logo" height="60" />
         </Navbar.Brand>
       </NavLink>
       <Navbar.Toggle
+        ref={ref}
         className={styles.Toggler}
         aria-controls="basic-navbar-nav"
+        onClick={() => setExpanded(!expanded)}
       />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto text-right">
