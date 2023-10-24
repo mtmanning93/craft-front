410c9c2f99d33a5b97e4146918d4ea99c2201d7a
Matt Manning <mtmanning93@hotmail.co.uk>
2023-10-24 09:11:14 +0000

    Update nav toggler to change on user login

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "../styles/NavBar.module.css";
-import { Nav, Navbar, Button } from "react-bootstrap";
+import { Nav, Navbar, Button, NavDropdown } from "react-bootstrap";
 import logo from "../assets/main_logo.png";
 import { NavLink } from "react-router-dom";
 import {
@@ -12,11 +12,10 @@ import axios from "axios";
 import ClickOutsideToggle from "../hooks/ClickOutsideToggle";
 
 const NavBar = () => {
-
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
 
-  const {expanded, setExpanded, ref} = ClickOutsideToggle();
+  const { expanded, setExpanded, ref } = ClickOutsideToggle();
 
   const handleLogOut = async () => {
     try {
@@ -26,6 +25,30 @@ const NavBar = () => {
       console.log(error);
     }
   };
+
+  const loggedInToggler = (
+    <Navbar.Toggle
+      ref={ref}
+      className={`ml-auto ${styles.ProfileToggler}`}
+      aria-controls="basic-navbar-nav"
+      onClick={() => setExpanded(!expanded)}
+    >
+      <Avatar
+        src={currentUser?.profile_image}
+        text={currentUser?.username}
+        height={40}
+      />
+    </Navbar.Toggle>
+  );
+
+  const loggedOutToggler = (
+    <Navbar.Toggle
+      ref={ref}
+      className={`ml-auto ${styles.Toggler}`}
+      aria-controls="basic-navbar-nav"
+      onClick={() => setExpanded(!expanded)}
+    />
+  );
 
   const loggedOutLinks = (
     <>
@@ -58,32 +81,58 @@ const NavBar = () => {
 
   const loggedInLinks = (
     <>
-      <NavLink to="/" onClick={handleLogOut} className={styles.Link}>
-        logout
-      </NavLink>
+      <NavDropdown
+        menuAlign="right"
+        title={
+          <Avatar
+            src={currentUser?.profile_image}
+            text={currentUser?.username}
+            height={50}
+          />
+        }
+        className={`d-none d-sm-block ${styles.UserDropdown}`}
+      >
+        <NavLink
+          to={`/profiles/${currentUser?.profile_id}`}
+          onClick={() => {}}
+          className={styles.Link}
+          activeClassName={styles.Active}
+        >
+          Profile
+        </NavLink>
+        <NavDropdown.Divider />
+        <NavLink to="/" onClick={handleLogOut} className={styles.Link}>
+          Logout
+        </NavLink>
+      </NavDropdown>
+
+      {/* small screeens */}
       <NavLink
         to={`/profiles/${currentUser?.profile_id}`}
         onClick={() => {}}
-        className={styles.Link}
+        className={`d-block d-sm-none ${styles.Link}`}
+        activeClassName={styles.Active}
       >
-        <Avatar src={currentUser?.profile_image} text={currentUser?.username} height={40} />
+        Profile
+      </NavLink>
+      <NavLink
+        to="/"
+        onClick={handleLogOut}
+        className={`d-block d-sm-none ${styles.Link}`}
+      >
+        Logout
       </NavLink>
     </>
   );
 
   return (
-    <Navbar expanded={expanded} expand="md" className={`${styles.NavBar}`}>
+    <Navbar expanded={expanded} expand="md" className={styles.NavBar}>
       <NavLink to="/">
         <Navbar.Brand className="mx-auto">
-          <img src={logo} alt="logo" height="60" />
+          <img src={logo} alt="logo" height="60" className={styles.Logo} />
         </Navbar.Brand>
       </NavLink>
-      <Navbar.Toggle
-        ref={ref}
-        className={styles.Toggler}
-        aria-controls="basic-navbar-nav"
-        onClick={() => setExpanded(!expanded)}
-      />
+      {currentUser ? loggedInToggler : loggedOutToggler}
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto text-right">
           {currentUser ? loggedInLinks : loggedOutLinks}
