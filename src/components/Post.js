627b76f9df52075649919187e76bba9eb297cc54
Matt Manning <mtmanning93@hotmail.co.uk>
2023-10-26 12:31:13 +0000

    Implement like and unlike functionality

--- src/components/Post.js
+++ src/components/Post.js
@@ -6,6 +6,7 @@ import Avatar from "./Avatar";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import mainStyles from "../App.module.css";
 import BackButton from "../components/buttons/BackButton";
+import { axiosRes } from "../api/axiosDefaults";
 
 const Post = (props) => {
   const {
@@ -22,11 +23,44 @@ const Post = (props) => {
     content,
     image,
     updated_on,
+    setPosts,
   } = props;
 
   const currentUser = useCurrentUser();
 
   const is_owner = currentUser?.username === owner;
+
+  const unlikePost = async () => {
+    try {
+      await axiosRes.delete(`/likes/${like_id}/`);
+      setPosts((prevPosts) => ({
+        ...prevPosts,
+        results: prevPosts.results.map((post) => {
+          return post.id === id
+            ? { ...post, likes_count: post.likes_count - 1, like_id: null }
+            : post;
+        }),
+      }));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const likePost = async () => {
+    try {
+      const { data } = await axiosRes.post("/likes/", { post: id });
+      setPosts((prevPosts) => ({
+        ...prevPosts,
+        results: prevPosts.results.map((post) => {
+          return post.id === id
+            ? { ...post, likes_count: post.likes_count + 1, like_id: data.id }
+            : post;
+        }),
+      }));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <Card className={`${styles.Post} ${mainStyles.Content}`}>
@@ -69,17 +103,17 @@ const Post = (props) => {
         <Card.Img className={styles.Img} src={image} alt={title} />
       </Link>
       <Card.Footer className="d-flex align-items-center justify-content-end">
-        <div className={styles.LikeComment}>
+        <div className={styles.Controls}>
+        <span className={styles.Count}>{comments_count}</span>
           <OverlayTrigger
             placement="bottom"
-            overlay={<Tooltip>Join the discusion.</Tooltip>}
+            overlay={<Tooltip>Join the discusion below.</Tooltip>}
           >
             <span className={styles.CommentIcon}>
-              {comments_count}
               <i class="fa-solid fa-message fa-sm"></i>
             </span>
           </OverlayTrigger>
-          {likes_count}
+          <span className={styles.Count}>{likes_count}</span>
           {/* {is_owner ? (
             <OverlayTrigger
               placement="bottom"
@@ -89,11 +123,11 @@ const Post = (props) => {
             </OverlayTrigger>
           ) : */}
           {like_id ? (
-            <span onClick={() => {}}>
+            <span className={styles.LikeIcon} onClick={unlikePost}>
               <i class="fa-solid fa-thumbs-up"></i>
             </span>
           ) : currentUser ? (
-            <span onClick={() => {}}>
+            <span className={styles.LikeIcon} onClick={likePost}>
               <i class="fa-regular fa-thumbs-up"></i>
             </span>
           ) : (
--- src/pages/pages/PostDetails.js
+++ src/pages/pages/PostDetails.js
@@ -8,15 +8,15 @@ const PostDetails = () => {
   const { id } = useParams();
 
   // empty array means you an fetch results or result
-  const [obj, setObj] = useState({ results: [] });
+  const [post, setPost] = useState({ results: [] });
 
   const handleMount = async () => {
     try {
-      const [{data: obj}] = await Promise.all([
+      const [{data: post}] = await Promise.all([
         axiosReq.get(`/posts/${id}`),
       ])
-      setObj({results: [obj]})
-      console.log(obj)
+      setPost({results: [post]})
+      console.log(post)
     } catch (err) {
       console.log(err);
     }
@@ -30,7 +30,7 @@ const PostDetails = () => {
     <Row className="w-100 border p-2">
       <Col className="border" lg={8}>
         <p>WOTW Mobile</p>
-        <Post {...obj.results[0]} setPost={setObj} />
+        <Post {...post.results[0]} setPosts={setPost} />
         <Container className="border">Comments</Container>
       </Col>
       <Col className="border d-none d-lg-block" lg={4}>
