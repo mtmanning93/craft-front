bb8b3195785b7e7b9d805aa66bf09b6956a4e916
Matt Manning <mtmanning93@hotmail.co.uk>
2023-11-01 10:04:14 +0000

    Add logic to eployer selector component

--- src/components/tools/EmployerSelector.js
+++ src/components/tools/EmployerSelector.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Select from "react-select/dist/declarations/src/Select";
+import { axiosReq } from "../../api/axiosDefaults";
 
 const EmployerSelector = () => {
-	return (
+	const [selectedCompany, setSelectedCompany] = useState(value);
+	const [companies, setCompanies] = useState([]);
 
-        <Select options={options} />
+	useEffect(() => {
+		const getCompanies = async () => {
+			try {
+				const { data } = await axiosReq.get("/companies");
+				const options = data.map((company) => ({
+					value: company.pk,
+					label: `${company.name} (${company.location})`,
+				}));
+				setCompanies(options);
+			} catch (error) {
+				console.log(error);
+			}
+		};
 
-    );
+        getCompanies();
+	}, []);
+
+	return <Select 
+        name="company"
+        value={selectedCompany}
+        onChange={(selectedOption) => {
+            setSelectedCompany(selectedOption);
+            onChange(selectedOption);
+        }}
+        options={companies}
+        isSearchable
+        placeholder="Select an employer"
+    />;
 };
 
 export default EmployerSelector;
