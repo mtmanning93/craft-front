e802edf10f645495b73e81b6c4a982b7f53be5b1
Matt Manning <mtmanning93@hotmail.co.uk>
2023-11-01 09:48:19 +0000

    Install react-select and create employer selector component

---
+++ src/components/tools/EmployerSelector.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Select from "react-select/dist/declarations/src/Select";
+
+const EmployerSelector = () => {
+	return (
+
+        <Select options={options} />
+
+    );
+};
+
+export default EmployerSelector;
