f6ac5d9cc3bb80394280aec15743c3615d2ef0f3
Matt Manning <mtmanning93@hotmail.co.uk>
2023-11-05 10:55:44 +0000

    Add styling to error page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,16 +1,21 @@
 import React from "react";
 import BackButton from "../components/buttons/BackButton";
-import { Container } from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import errorCone from "../assets/error_cone.jpg";
+import mainStyles from "../App.module.css";
 
 const ErrorPage = () => {
 	return (
-		<Container fluid className="d-flex flex-column text-center mt-3">
-			<h1>Resource Not Found</h1>
-			<p>Click the button below to go back to previous page.</p>
-			<div>
-			    <BackButton />
-			</div>
-		</Container>
+		<Card className={`${mainStyles.Content} text-center m-3 p-2`}>
+			<Card.Img variant="top" src={errorCone}/>
+			<Card.Body>
+				<Card.Title className="border-bottom border-dark pb-2" as="h1">Resource Not Found</Card.Title>
+				<Card.Text>
+					Click the button below to go back to previous page.
+				</Card.Text>
+				<BackButton />
+			</Card.Body>
+		</Card>
 	);
 };
 
