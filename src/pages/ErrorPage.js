7e26b1fa59ba7bdbb0e7345a2480dabe6d43ccc7
Matt Manning <mtmanning93@hotmail.co.uk>
2023-11-14 14:53:49 +0000

    Update button

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,19 +1,25 @@
 import React from "react";
-import BackButton from "../components/buttons/BackButton";
+import MainButton from "../components/buttons/MainButton";
 import Card from "react-bootstrap/Card";
 import errorCone from "../assets/error_cone.jpg";
 import mainStyles from "../App.module.css";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const ErrorPage = () => {
+    const history = useHistory();
+
+    const goHome = () => {
+        history.push("/")
+    }
 	return (
 		<Card className={`${mainStyles.Content} text-center m-3 p-2`}>
 			<Card.Img variant="top" src={errorCone} alt="Error page cone" />
-			<Card.Body>
+			<Card.Body className="d-flex flex-column justify-content-center">
 				<Card.Title className="border-bottom border-dark pb-2" as="h1">Resource Not Found</Card.Title>
 				<Card.Text>
-					Click the button below to go back to previous page.
+					Click the button below to go home.
 				</Card.Text>
-				<BackButton />
+				<MainButton type="button" onClick={goHome} text="Go Home" />
 			</Card.Body>
 		</Card>
 	);
