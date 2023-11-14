ca1ac08f2721aa6565f51e67410e0c8b47007ed9
Matt Manning <mtmanning93@hotmail.co.uk>
2023-11-14 13:24:47 +0000

    Add condition to rendered company label

--- src/components/tools/EmployerSelector.js
+++ src/components/tools/EmployerSelector.js
@@ -3,42 +3,43 @@ import Select from "react-select";
 import { axiosReq } from "../../api/axiosDefaults";
 
 const EmployerSelector = ({ value, onChange }) => {
-
 	const [companies, setCompanies] = useState([]);
 	const [error, setError] = useState(null);
 
-    useEffect(() => {
-        let isMounted = true;
-    
-        const getCompanies = async () => {
-          try {
-            const response = await axiosReq.get("/companies/");
-            const data = response.data.results;
-    
-            const options = data.map((company) => ({
-              value: company.id,
-              label: `${company.name} (${company.location})`,
-            }));
-    
-            options.unshift({ value: "", label: "---------" });
-    
-            if (isMounted) {
-              setCompanies(options);
-              setError(null);
-            }
-          } catch (err) {
-            if (isMounted) {
-              setError("Currently unable to retrieve company list");
-            }
-          }
-        };
-    
-        getCompanies();
-    
-        return () => {
-          isMounted = false;
-        };
-      }, []);
+	useEffect(() => {
+		let isMounted = true;
+
+		const getCompanies = async () => {
+			try {
+				const response = await axiosReq.get("/companies/");
+				const data = response.data.results;
+
+				const options = data.map((company) => ({
+					value: company.id,
+					label: company.location
+						? `${company.name} (${company.location})`
+						: company.name,
+				}));
+
+				options.unshift({ value: "", label: "---------" });
+
+				if (isMounted) {
+					setCompanies(options);
+					setError(null);
+				}
+			} catch (err) {
+				if (isMounted) {
+					setError("Currently unable to retrieve company list");
+				}
+			}
+		};
+
+		getCompanies();
+
+		return () => {
+			isMounted = false;
+		};
+	}, []);
 
 	if (error) {
 		return <p className="text-danger">{error}</p>;
