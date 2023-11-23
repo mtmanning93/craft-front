ee785aa0ebe0d7fd0be2bc1bf73fc898fd1eb8e5
Matt Manning <mtmanning93@hotmail.co.uk>
2023-11-23 13:21:19 +0000

    Add comments to api folder

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+/**
+ * REST API Configuration
+ */
+
 axios.defaults.baseURL = "https://craft-api-aeec93e46ff2.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
+// Axios instances to be used as interceptors
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
